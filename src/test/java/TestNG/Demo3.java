66836883e3f2bad2c22373bace98b18a9f1d01aa
AntonSemenov <Anthony04061987@gmail.com>
2020-04-09 23:51:10 -0500

    TCs updates

--- src/test/java/TestNG/Demo3.java
+++ src/test/java/TestNG/Demo3.java
@@ -7,30 +7,30 @@ public class Demo3 {
     @Test(priority = 1)
     public void WebloginCarLoan () {
         // Selenium
-        System.out.println("Weblogincar");
+        System.out.println("Demo 3 -Weblogincar");
     }
 
     @Test (priority = 2)
     public void MobileloginCarLoan () {
         // Appium
-        System.out.println("Mobilelogincar");
+        System.out.println("Demo 3 -Mobilelogincar");
     }
 
     @Test (priority = 3)
     public void MobilelogOutCarLoan () {
         // Appium
-        System.out.println("MobilelogOutcar");
+        System.out.println("Demo 3 - MobilelogOutcar");
     }
 
     @Test (priority = 4)
     public void MobileSignUpCarLoan () {
         // Appium
-        System.out.println("MobileSignUpcar");
+        System.out.println("Demo 3 - MobileSignUpcar");
     }
 
     @Test (priority = 5)
     public void LoginAPICarLoan () {
         // Rest API automation
-        System.out.println("APILogincar");
+        System.out.println("Demo 3 - APILogincar");
     }
 }

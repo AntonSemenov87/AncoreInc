d6ddb5e4996b2cd21939d45436b2acde44f9592c
AntonSemenov <Anthony04061987@gmail.com>
2020-04-18 16:42:09 -0500

    SureFire plugin

---
+++ src/test/java/SureFirePlugin_test/FakeAppiumTest.java
@@ -0,0 +1,11 @@
+package SureFirePlugin_test;
+
+import org.testng.annotations.Test;
+
+public class FakeAppiumTest {
+
+    @Test
+    public void appium () {
+        System.out.println("This is fake Appium test");
+    }
+}

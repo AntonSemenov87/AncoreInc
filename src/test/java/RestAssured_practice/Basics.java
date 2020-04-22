a20767a1e7841d007b15da843ebba5f9a0962446
AntonSemenov <Anthony04061987@gmail.com>
2020-04-22 03:47:58 -0500

    API

--- src/test/java/RestAssured_practice/Basics.java
+++ src/test/java/RestAssured_practice/Basics.java
@@ -1,6 +1,10 @@
 package RestAssured_practice;
 
+
+import files.Payload;
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static  org.hamcrest.Matchers.*;
 
 public class Basics {
     public static void main(String[] args) {
@@ -8,9 +12,18 @@ public class Basics {
         // validate if Add Place API is working
         // given, when, then
         // given --> all input details
-        // when --> submit the API
+        // when --> submit the API -- resource and http methods
         // then --> validate the response
 
+        // Step 1 - create a place
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").
+                body(Payload.addPlace()).when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.18 (Ubuntu)");
+
+
+        // Step 2 - update a place, get place_id number and re-use it
 
 
     }
---
+++ src/test/java/files/Payload.java
@@ -0,0 +1,23 @@
+package files;
+
+public class Payload {
+
+    public static String addPlace () {
+        return "{\\n\" +\n" +
+                "                        \"  \\\"location\\\": {\\n\" +\n" +
+                "                        \"    \\\"lat\\\": -38.383494,\\n\" +\n" +
+                "                        \"    \\\"lng\\\": 33.427362\\n\" +\n" +
+                "                        \"  },\\n\" +\n" +
+                "                        \"  \\\"accuracy\\\": 50,\\n\" +\n" +
+                "                        \"  \\\"name\\\": \\\"Anton House\\\",\\n\" +\n" +
+                "                        \"  \\\"phone_number\\\": \\\"(+91) 983 893 3937\\\",\\n\" +\n" +
+                "                        \"  \\\"address\\\": \\\"7226 W Belden ave\\\",\\n\" +\n" +
+                "                        \"  \\\"types\\\": [\\n\" +\n" +
+                "                        \"    \\\"shoe park\\\",\\n\" +\n" +
+                "                        \"    \\\"shop\\\"\\n\" +\n" +
+                "                        \"  ],\\n\" +\n" +
+                "                        \"  \\\"website\\\": \\\"http://www.flipchitown.com\\\",\\n\" +\n" +
+                "                        \"  \\\"language\\\": \\\"Russian-EN\\\"\\n\" +\n" +
+                "                        \"}";
+    }
+}

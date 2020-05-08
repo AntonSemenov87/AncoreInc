af371b935b9378daddbea5167844e17abe36a2a1
AntonSemenov <Anthony04061987@gmail.com>
2020-05-07 21:11:34 -0500

    test

---
+++ src/test/java/Stuff/anotherOne.java
@@ -0,0 +1,9 @@
+package Stuff;
+
+import java.util.Calendar;
+
+public class anotherOne {
+    public static void main(String[] args) {
+        System.out.println(Calendar.getInstance().getTime());
+    }
+}
